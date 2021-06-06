517f19ebed22beb6ea131dbd63697a025b350258
dandy <danielsbarbosa27@gmail.com>
2021-06-06 02:34:29 -0300

    buidButtonCart

--- dandy.com/lib/common/components/body_card.dart
+++ dandy.com/lib/common/components/body_card.dart
@@ -4,6 +4,8 @@ import 'package:pizzaorders2/common/components/pizza_ingredients.dart';
 
 import '../../constants.dart';
 
+const _pizzaCardSize = 50.0;
+
 class BodyCard extends StatelessWidget {
   const BodyCard({
     Key? key,
@@ -30,8 +32,63 @@ class BodyCard extends StatelessWidget {
               ],
             ),
           ),
+        ),
+        Positioned(
+          bottom: 40,
+          height: _pizzaCardSize,
+          width: _pizzaCardSize,
+          left: MediaQuery.of(context).size.width / 2 - _pizzaCardSize / 2,
+          child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.orange.withOpacity(0.5),
+                    Colors.orange,
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.shopping_cart_outlined,
+                ),
+                color: Colors.white,
+                iconSize: 30,
+              )),
         )
       ],
     );
   }
 }
+
+// Widget buildButtonCart(
+//         {required BuildContext context,
+//         required VoidCallback press,
+//         required Icon icon}) =>
+//     Positioned(
+//       bottom: 35,
+//       height: 50,
+//       width: 50,
+//       left: MediaQuery.of(context).size.width / 2 - 50 / 2,
+//       child: Container(
+//           decoration: BoxDecoration(
+//             gradient: LinearGradient(
+//               begin: Alignment.topCenter,
+//               end: Alignment.bottomCenter,
+//               colors: [
+//                 Colors.orange.withOpacity(0.5),
+//                 Colors.orange,
+//               ],
+//             ),
+//             borderRadius: BorderRadius.circular(5),
+//           ),
+//           child: IconButton(
+//             onPressed: press,
+//             icon: icon,
+//             color: Colors.white,
+//             iconSize: 35,
+//           )),
+//     );
